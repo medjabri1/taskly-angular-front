49a65d93d5d6a42ae4a07a57605497a781435783
Mohammed JABRI <mohammed.jabri2000@gmail.com>
2023-08-07 14:53:22 +0100

    Moving sidebar and header to separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BoardComponent } from './pages/board/board.component';
 import { CalendarComponent } from './pages/calendar/calendar.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { CalendarComponent } from './pages/calendar/calendar.component';
     HomeComponent,
     BoardComponent,
     CalendarComponent,
+    SidebarComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
