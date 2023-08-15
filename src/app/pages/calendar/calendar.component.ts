68c1e05b9b9a8e56c48c44f3f99c1d90b533b9ad
Mohammed JABRI <mohammed.jabri2000@gmail.com>
2023-08-15 16:49:53 +0100

    Calendar Page added

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class CalendarComponent {
 
+  teams: Array<String> = [
+    'UI / UX Design',
+    'Illustration Design',
+    'Marketplace',
+    'Nike Landing',
+    'Bank of America',
+    'Visa',
+    'Rebook Run App',
+    'Landing for education',
+    'Design team',
+  ]
+
+  dates: Array<Number> = [];
+
+
+  day = new Date().getDate();
+  month = new Date().getMonth();
+
+  shift = 0;
+
+  ngOnInit() {
+    this.fillDates();
+
+    let i = 0;
+  }
+  
+  fillDates() {  
+    this.dates = [];
+    for(let i = this.day - 2 + this.shift; i <= this.day + 4 + this.shift; i++) {
+      this.dates.push(i);
+    }
+  }
+
+  handleShiftDate(step: number) {
+    this.shift += step;
+    this.fillDates();
+  }
+
 }
